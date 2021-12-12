f12fc1277e6638afbbd1fe87438aa11037cad0c4
NIC619 <twedusuck@gmail.com>
2021-12-12 17:41:59 +0800

    Add weightedCalculate

--- src/RebalanceRewardCalculation.sol
+++ src/RebalanceRewardCalculation.sol
@@ -17,6 +17,11 @@ contract RebalanceRewardCalculation is Ownable {
     uint256 constant private BPS = 10000;
     // Minimum reward rate that must be met in order to give out the reward
     uint256 public REBALANCE_REWARD_RATE_THRESHOLD;
+    // Scale down PCV so exponentiation of PCVs does not result in overflow
+    // Assume PCV has decimals of 18
+    // Assume PCV and amount in/out must be greater than 10 ** 18, otherwise PCV product would result in 0 and hence 0 reward
+    uint256 constant private productScaleFactor = 10 ** 18;
+    uint256 constant public MAX_CHAIN_WEIGHT = 4;
 
 
     /* ========== CONSTRUCTOR ========== */
@@ -84,6 +89,84 @@ contract RebalanceRewardCalculation is Ownable {
         if (prevFromChainPCV != 0) {
             // If source chain had liquidity, multiplies it
             oldFromToPCVProduct *= prevFromChainPCV;
+        }
+
+        // Step 3-1. check if new liquidity product is greater than old liquidity product which implies the balance between each chain being improved
+        // Reward only if liquidity product increases
+        if (newFromToPCVProduct <= oldFromToPCVProduct) {
+            rebalanceRewardRate = 0;
+        } else {
+            // Multiplied by the ratio of liquidity product increase relative to the old liquidity product
+            rebalanceRewardRate = rebalanceRewardRate * (newFromToPCVProduct - oldFromToPCVProduct) / oldFromToPCVProduct;
+        }
+        // Step 3-2. bound the reward rate
+        rebalanceRewardRate = min(BPS, rebalanceRewardRate);
+
+        // Step 4. only give out reward if reward rate met the threshold
+        if (rebalanceRewardRate >= REBALANCE_REWARD_RATE_THRESHOLD) {
+            // Base reward is `swapFeeAmount`
+            // On top of top, another percentage of `swapFeeAmount` is added depending on the reward rate
+            // So the reward amount is between `swapFeeAmount` and `2 * swapFeeAmount`
+            rebalanceReward = swapFeeAmount + swapFeeAmount * rebalanceRewardRate / BPS;
+        }
+
+        // Step 5. convert reward amount to XY Token amount because reward amount is denominated in the YPool token while reward given out should be XY token
+        rebalanceReward = rebalanceReward * (10 ** xyUSDValueDecimals) / xyTokenUSDValue;
+
+        return (oldFromToPCVProduct, newFromToPCVProduct, rebalanceRewardRate, rebalanceReward);
+    }
+
+    /// @notice This function calculates rebalance reward amount the user can get if the swap is settled
+    /// @dev This function emits `RebalanceRewardAdded` event including info like old and new source/target chain liquidity product and new reward rate
+    /// `xyTokenUSDValue` MUST be greater than zero
+    /// @param xyUSDValueDecimals Decimal of XY token value in USD
+    /// @param swapFeeAmount Fee amount the user is paying for the swap
+    /// @param xyTokenUSDValue XY token value in USD
+    /// @param prevTotalPCV Total liquidity before the swap is settled
+    /// @param prevFromChainPCV Source chain liquidity before the swap is settled
+    /// @param fromChainWeight Weight of source chain when calculating PCV product
+    /// @param amountIn Amount of YPool token sent to YPool vault on source chain
+    /// @param prevToChainPCV Target chain liquidity before the swap is settled
+    /// @param toChainWeight Weight of target chain when calculating PCV product
+    /// @param amountOut Amount of YPool token transfered away from YPool vault on target chain
+    /// @return oldFromToPCVProduct Product of source chain liqudity and target chain liquidity before the swap is settled
+    /// @return newFromToPCVProduct Product of source chain liqudity and target chain liquidity after the swap is settled
+    /// @return rebalanceRewardRate Reward rate of the swap
+    /// @return rebalanceReward Reward given to the user
+    function weightedCalculate(
+        uint256 xyUSDValueDecimals,
+        uint256 swapFeeAmount,
+        uint256 xyTokenUSDValue,
+        uint256 prevTotalPCV,
+        uint256 prevFromChainPCV,
+        uint256 fromChainWeight,
+        uint256 amountIn,
+        uint256 prevToChainPCV,
+        uint256 toChainWeight,
+        uint256 amountOut
+    ) external view returns (uint256 oldFromToPCVProduct, uint256 newFromToPCVProduct, uint256 rebalanceRewardRate, uint256 rebalanceReward) {
+        require(
+            0 < fromChainWeight && fromChainWeight <= MAX_CHAIN_WEIGHT && 0 < toChainWeight && toChainWeight <= MAX_CHAIN_WEIGHT,
+            "ERR_INVALID_CHAIN_WEIGHT"
+        );
+
+        // Step 1. calculate the initial reward rate
+        // Scale down reward rate base on the size of source chain liquidity, target chain liquidity and total liquidity
+        // The smaller the sum of source chain liquidity and target chain liquidity, the smaller the reward rate
+        rebalanceRewardRate = BPS * (prevFromChainPCV + prevToChainPCV) / prevTotalPCV;
+
+        // Step 2. calculate the product of sourchain liquidity and target chain liquidity, both old and new
+        // `prevFromChainPCV + amountIn` MUST be greater than zero
+        newFromToPCVProduct = ((prevFromChainPCV + amountIn) / productScaleFactor) ** fromChainWeight;
+        if (prevToChainPCV > amountOut) {
+            // If target chain has liquidity left, multiplies it
+            newFromToPCVProduct *= ((prevToChainPCV - amountOut) / productScaleFactor) ** toChainWeight;
+        }
+        // `prevToChainPCV` MUST be greater than zero
+        oldFromToPCVProduct = (prevToChainPCV / productScaleFactor) ** toChainWeight;
+        if (prevFromChainPCV != 0) {
+            // If source chain had liquidity, multiplies it
+            oldFromToPCVProduct *= (prevFromChainPCV / productScaleFactor) ** fromChainWeight;
         }
 
         // Step 3-1. check if new liquidity product is greater than old liquidity product which implies the balance between each chain being improved
